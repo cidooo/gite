472ccac4c93512f8801c3998c8774f36e76a0aaf
cidooo <cigdemfiratt@gmail.com>
2021-06-07 23:47:30 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static String[] veriler = new String[0];
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Add(textBox1.Text);
+            Array.Resize(ref veriler, veriler.Length + 1);
+            veriler[veriler.GetUpperBound(0)] = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 fr2 = new Form2();
+            fr2.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gite
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Form1.veriler.Length; i++)
+
+            { listBox1.Items.Add(Form1.veriler[i]); }
+        }
+    }
+}
